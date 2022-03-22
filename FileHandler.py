516ef32d488d6ccab5bddcebd31065c19d3da23c
Axel Garcia <axelegar@gmail.com>
2022-03-22 12:52:20 -0700

    FileHandler finished.
    Signed-off-by: Axel Garcia <axelegar@gmail.com>

--- FileHandler.py
+++ FileHandler.py
@@ -1,5 +1,6 @@
 from distutils.command.config import config
 import json
+from unittest import result
 import drain3
 import logging
 import os
@@ -28,3 +29,32 @@ lineCount = 0
 with open(inLogFile) as f:
     lines = f.readlines()
 
+startTime = time.time()
+procStartTime = startTime
+procSize = 10000
+
+for line in lines:
+    line = line.rstrip()
+    line = line.partition(']')[2]
+    result = template_miner.add_log_message(line)
+    lineCount += 1
+    if lineCount % procSize == 0:
+        timeTaken = time.time() - procStartTime
+        rate = procSize / timeTaken
+        logger.info(f"Processing line: {lineCount}, rate {rate:.1f} lines/sec, " f"{len(template_miner.drain.clusters)} clusters so far.")
+        procStartTime = time.time()
+    if result["change_type"] != "none":
+        resultJSON = json.dumps(result)
+        logger.info(f"Input ({lineCount}): " + line)
+        logger.info("Result: " + resultJSON)
+
+timeTaken = time.time() - startTime
+rate = lineCount / timeTaken
+logger.info(f"--- Done processing file in {timeTaken:.2f} sec. Total of {lineCount} lines, rate {rate:.1f} lines/sec, " f"{len(template_miner.drain.clusters)} clusters")
+
+sortedClusters = sorted(template_miner.drain.clusters, key=lambda it: it.size, reverse=True)
+for cluster in sortedClusters:
+    logger.info(cluster)
+print("Prefix Tree:")
+template_miner.drain.print_tree()
+template_miner.profiler.report(0)
