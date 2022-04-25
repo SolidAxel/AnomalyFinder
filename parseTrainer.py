d0cc8fb514fd173d9ce1cae4793ca440e894f0ed
Axel Garcia <axel.garcia@positioninguniversal.com>
2022-04-25 14:12:23 -0700

    Add parameter extraction

--- parseTrainer.py
+++ parseTrainer.py
@@ -4,7 +4,6 @@ from posixpath import abspath
 from unittest import result
 import logging
 import os
-import subprocess
 import sys
 import time
 
@@ -16,17 +15,19 @@ from drain3.file_persistence import FilePersistence
 persistence = FilePersistence("states/drain3State.bin")
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(stream=sys.stdout, level=logging.INFO,format='%(message)s')
+logging.basicConfig(
+    stream=sys.stdout, level=logging.INFO, format="%(message)s"
+)
 
-fileHandler = logging.FileHandler('Output.log')
+fileHandler = logging.FileHandler("Output.log")
 fileHandler.setLevel(logging.INFO)
 
 logger.addHandler(fileHandler)
 
 inLogFile = input("Please enter filename>> ")
 if not os.path.isfile(inLogFile):
-    logger.info('Make sure file is in same directory as this program.')
-    Exception('File not found in current directory.')
+    logger.info("Make sure file is in same directory as this program.")
+    Exception("File not found in current directory.")
 
 config = TemplateMinerConfig()
 config.load(dirname(os.path.abspath(__file__)) + "/drainConfig.ini")
@@ -41,30 +42,44 @@ with open(inLogFile) as f:
 startTime = time.time()
 procStartTime = startTime
 procSize = 10000
+params = []
 
 for line in lines:
     line = line.rstrip()
-    line = line.partition(']')[2]
+    line = line.partition("] ")[2]
     result = templateMiner.add_log_message(line)
+    parameters = templateMiner.extract_parameters(
+        result["template_mined"], line, exact_matching=True
+    )
+    if parameters:
+        params.append(parameters)
     lineCount += 1
     if lineCount % procSize == 0:
         timeTaken = time.time() - procStartTime
         rate = procSize / timeTaken
-        logger.info(f"Processing line: {lineCount}, rate {rate:.1f} lines/sec, " f"{len(templateMiner.drain.clusters)} clusters so far.")
+        logger.info(
+            f"Processing line: {lineCount}, rate {rate:.1f} lines/sec, "
+            f"{len(templateMiner.drain.clusters)} clusters so far."
+        )
         procStartTime = time.time()
     if result["change_type"] != "none":
         resultJSON = json.dumps(result)
         logger.info(f"Input ({lineCount}): " + line)
         logger.info("Result: " + resultJSON)
-        
 
 
 timeTaken = time.time() - startTime
 rate = lineCount / timeTaken
-logger.info(f"--- Done processing file in {timeTaken:.2f} sec. Total of {lineCount} lines, rate {rate:.1f} lines/sec, " f"{len(templateMiner.drain.clusters)} clusters")
-sortedClusters = sorted(templateMiner.drain.clusters, key=lambda it: it.size, reverse=True)
+logger.info(
+    f"--- Done processing file in {timeTaken:.2f} sec. Total of {lineCount} lines, rate {rate:.1f} lines/sec, "
+    f"{len(templateMiner.drain.clusters)} clusters"
+)
+sortedClusters = sorted(
+    templateMiner.drain.clusters, key=lambda it: it.size, reverse=True
+)
 for cluster in sortedClusters:
     logger.info(cluster)
 print("Prefix Tree:")
 templateMiner.drain.print_tree()
 templateMiner.profiler.report(0)
+print(str(params))
